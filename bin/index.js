1836d2f93d0598fb0b15f42b2033093ebe7f7ea0
razvan232432 <marius3837@proton.me>
2023-06-30 11:24:43 -0700

    Update index.js

--- bin/index.js
+++ bin/index.js
@@ -1,8 +1,23 @@
 const yargs = require('yargs');
+const chalk = require('chalk');  
+const boxen = require('boxen');
+
+// create options
 
 const options = yargs
 .usage(usage)
-.option("l", {alias:"language", describe: "Translate to language", type: "string", demandOption: false })
+.option("l", {alimyas:"language", describe: "Translate to language", type: "string", demandOption: false })
 .option("s", {alias:"sentence", describe: "Sentence to be translated", type: "string", demandOption: false })
 .help(true)
 .argv;
+
+//color  and  display output
+
+const usage = chalk.keyword('violet')("\nUsage: mycli -l <language>  -s <sentence> \n"
++ boxen(chalk.green("\n" + "Translates a sentence to specific language" + "\n"), {padding: 1, borderColor: 'green', dimBorder: true}) + "\n");
+
+translate(sentence, {to: language}).then(res => {console.log("\n" + boxen(chalk.green("\n" + res.text + "\n"
+),
+{padding: 1, borderColor: 'green', dimBorder: true}) + "\n");}).catch(err => {                            
+     console.error(err);  
+ });
